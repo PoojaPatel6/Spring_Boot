5b13f38b9602145ae5607516ccf1ddd98b92acd8
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-08-07 15:34:38 +0530

    Spring Basics

---
+++ SpringbootProject2/src/main/java/in/sp/main/SpringbootProject2Application.java
@@ -0,0 +1,13 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject2Application.class, args);
+	}
+
+}
