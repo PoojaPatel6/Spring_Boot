84d69982a80f98cd08a3f564406826f945c02fc0
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-08-08 15:47:01 +0530

    Add files via upload
    Spring Boot Auto-Configuration

---
+++ SpringbootProject4/src/main/java/in/ibm/SpringbootProject4Application.java
@@ -0,0 +1,13 @@
+package in.ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject4Application.class, args);
+	}
+
+}
